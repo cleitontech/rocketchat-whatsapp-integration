1c1b2fb9e68845f233b7b65a46a05791038175c4
Kevin Paulo <kevinppaulo@gmail.com>
2020-07-02 21:22:14 -0300

    [wip] sending ptt

--- src/file_handlers/file_converter.py
+++ src/file_handlers/file_converter.py
@@ -5,11 +5,11 @@ import base64
 import requests
 
 def convert_mp3_ogg(filepath):
-    #TODO: delete temporary files
-    request = requests.get(filepath, allow_redirects = True)
+    #TODO: delete temp files
+    request = requests.get(filepath, allow_redirects=True)
     file_bytes = request.content
 
-    temp_file_name = "temp/"+str(uuid.uuid4()) + ".mp3"
+    temp_file_name = "temp/" + str(uuid.uuid4()) + ".mp3"
     temp_file = open(temp_file_name, "wb")
     temp_file.write(file_bytes)
     temp_file.close()
@@ -18,7 +18,7 @@ def convert_mp3_ogg(filepath):
     AudioSegment.from_mp3(temp_file_name).export('static/{}.ogg'.format(filename), format='ogg')
 
     data = open("static/{}.ogg".format(filename), "rb").read()
-    encoded = base64.b64decode(data).decode('ascii')
-    return encoded
+    encoded = base64.b64encode(data).decode('ascii')
 
+    return encoded
 
