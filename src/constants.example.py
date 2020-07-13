466fff2acccd2b3ba5416fcf9a5242ceac127003
Duda Nogueira <dudanogueira@gmail.com>
2020-07-13 17:47:59 -0300

    Added Environmentable constants.example.py

--- src/constants.example.py
+++ src/constants.example.py
@@ -1,32 +1,70 @@
 import os
 
-domain_name                         = "your-domain-name.com"
-MEDIA_UPLOAD_URL                    = "http://your-domain:port/static/media_upload/" 
-MEDIA_UPLOAD_PATH                    = os.path.join(os.getcwd(), "static/media_upload/") 
+domain_name = os.environ.get(
+    "domain_name", "your.domain.com"
+)
+MEDIA_UPLOAD_URL = os.environ.get(
+    "MEDIA_UPLOAD_URL", "https://{0}/static/media_upload/".format(domain_name)
+)
+
+MEDIA_UPLOAD_PATH = os.environ.get(
+    "MEDIA_UPLOAD_PATH", os.path.join(os.getcwd(), "static/media_upload/")
+)
 
 # CHAT API ENDPOINTS
-CHAT_API_URL                        = 'https://eu200.chat-api.com/instanceXXXXXX'
-INSTANCE_ID                         = 'XXXXXX'
-CHAT_API_TOKEN                      = 'your-api-token'
+CHAT_API_URL = os.environ.get(
+    "CHAT_API_URL", 'https://euXXX.chat-api.com/instanceXXXXXX/'
+)
+
+INSTANCE_ID = os.environ.get("INSTANCE_ID", 'XXXXXX')
+CHAT_API_TOKEN = os.environ.get("CHAT_API_TOKEN", 'kj1h23kj1h232')
 
 # check https://chat-api.com/en/docs_beta.html#/messages/getFile for more.
-CHAT_GET_FILE                       = "/getFile?msgId={}&token={}"
+CHAT_GET_FILE = os.environ.get("CHAT_GET_FILE", "/getFile?msgId={}&token={}")
 
 
 # ROCKET CHAT ENDPOINTS
-ROCKET_URL_PREFIX                   = 'http://your-rocket-chat-domain'
-ROCKET_MESSAGE_URL_POSTFIX          = '/api/v1/livechat/message'
-ROCKET_GET_ROOM_POSTFIX             = '/api/v1/livechat/room'
-ROCKET_VISITOR_POSTFIX              = "/api/v1/livechat/visitor"
+ROCKET_URL_PREFIX = os.environ.get(
+    "ROCKET_URL_PREFIX", 'http://your-rocket-chat-domain'
+)
+ROCKET_MESSAGE_URL_POSTFIX = os.environ.get(
+    "ROCKET_MESSAGE_URL_POSTFIX", '/api/v1/livechat/message'
+)
+ROCKET_GET_ROOM_POSTFIX = os.environ.get(
+    "ROCKET_GET_ROOM_POSTFIX", '/api/v1/livechat/room')
 
-STATIC_FILES_PATH                   = "http://localhost:5000/static/"
+ROCKET_VISITOR_POSTFIX = os.environ.get(
+    "ROCKET_VISITOR_POSTFIX", "/api/v1/livechat/visitor")
 
-CHAT_API_QUEUE_FOLDER               = "messages_queue/chat_api/"
-ROCKET_QUEUE_FOLDER                 = "messages_queue/rocket_chat/"
-CHAT_API_IDS                        = "messages_received_chat_api/"
 
-ROCKETCHAT_MESSAGE_BLACKLIST        = ["Chat encerrado pelo agente"]
+CHAT_API_QUEUE_FOLDER = os.environ.get(
+    "CHAT_API_QUEUE_FOLDER", "messages_queue/chat_api/"
+)
+
+ROCKET_QUEUE_FOLDER =  os.environ.get(
+    "ROCKET_QUEUE_FOLDER", "messages_queue/rocket_chat/"
+)
+
+CHAT_API_IDS = os.environ.get(
+    "CHAT_API_IDS", "messages_received_chat_api/"
+)
+
+ROCKETCHAT_MESSAGE_BLACKLIST = ["Chat encerrado pelo agente"]
 
 # SECRET TOKENS
-CHATAPI_WEBHOOK_TOKEN               = "A secret token you define as a get parameter of the chat api webhook" 
-ROCKETC_WEBHOOK_TOKEN               = "A secret token configured in the rocket chat live chat webhook." 
+CHATAPI_WEBHOOK_TOKEN = os.environ.get(
+    "CHATAPI_WEBHOOK_TOKEN", "secret1"
+)
+ROCKETC_WEBHOOK_TOKEN = os.environ.get(
+    "ROCKETC_WEBHOOK_TOKEN", "secret2"
+)
+
+# NO AGENT CONFIGS
+NO_AGENT_NAME = os.environ.get(
+    "NO_AGENT_NAME", "NO AGENTS"
+)
+NO_AGENT_MESSAGE = os.environ.get(
+    "NO_AGENT_MESSAGE",
+    '''Sorry! No Agents right now. We have registered your message and will
+get back as soon as possible'''
+)
